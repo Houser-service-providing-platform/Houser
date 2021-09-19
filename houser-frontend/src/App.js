cab69b1d0a92fba8ab45629de202cce8d5960846
raushanrk5 <kumarraushanrk5@gmail.com>
2021-09-19 22:57:19 +0530

    sigup functionalty done

--- houser-frontend/src/App.js
+++ houser-frontend/src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import signup from './components/signup';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>    
+        <div className="container">    
+          <nav className="navbar navbar-expand-lg navheader">    
+            <div className="collapse navbar-collapse" >    
+              <ul className="navbar-nav mr-auto">    
+                <li className="nav-item">    
+                  <Link to={'/Login'} className="nav-link">Login</Link>    
+                </li>    
+                <li className="nav-item">    
+                  <Link to={'/Signup'} className="nav-link">Sign Up</Link>    
+                </li>    
+                
+              </ul>    
+            </div>    
+          </nav> <br />    
+          <Switch>    
+            {/* <Route exact path='/Login' component={Login} />     */}
+            <Route path='/Signup' component={signup} />    
+          </Switch>    
+        </div>    
+      </Router>   
     </div>
   );
 }
