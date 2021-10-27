c636a68b2033fa1d84dbb5281a2c7b5403c34944
raushanrk5 <kumarraushanrk5@gmail.com>
2021-10-27 15:39:22 +0530

    installed some packages

--- houser-frontend/src/App.js
+++ houser-frontend/src/App.js
@@ -2,32 +2,40 @@ import './App.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import signup from './components/signup';
-import login from './components/login';
+import Login from './components/login';
+import home from './components/home';
+import PrivateRoute from './utils/PrivateRoute'
+import { AuthProvider } from './context/AuthContext'
 
 function App() {
   return (
     <div className="App">
       <Router>    
-        <div className="container">    
-          <nav className="navbar navbar-expand-lg navheader">    
+      <nav className="navbar navbar-expand-lg navheader">    
             <div className="collapse navbar-collapse" >    
               <ul className="navbar-nav mr-auto">    
                 <li className="nav-item">    
-                  <Link to={'/Login'} className="nav-link">Login</Link>    
+                  <Link to={'/login'} className="nav-link">Login</Link>    
                 </li>    
                 <li className="nav-item">    
-                  <Link to={'/Signup'} className="nav-link">Sign Up</Link>    
+                  <Link to={'/signup'} className="nav-link">Sign Up</Link>    
                 </li>    
                 
               </ul>    
             </div>    
-          </nav> <br />    
-          <Switch>    
+          </nav> <br /> 
+      <AuthProvider>
+        <div className="container">    
+          
+
+          
             {/* <Route exact path='/Login' component={Login} />     */}
-            <Route path='/Signup' component={signup} />  
-            <Route path='/Login' component={login} />    
-          </Switch>    
-        </div>    
+            <PrivateRoute path='/' component={home} />
+            <Route path='/signup' component={signup} />  
+            <Route path='/login' component={Login} />    
+          
+        </div>              
+        </AuthProvider>     
       </Router>   
     </div>
   );
