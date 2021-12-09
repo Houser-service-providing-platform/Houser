14a4bb8da49c7ae5c2f1f936af69f183b2a80b4d
Raushan Kumar <kumarraushanrk5@gmail.com>
2021-12-09 16:49:04 +0530

    Update App.js

--- houser-frontend/frontend/src/App.js
+++ houser-frontend/frontend/src/App.js
@@ -1,15 +1,14 @@
 import "./App.css";
 import Navbar from "./component/Navbar";
-import Crousel from "./component/Crousel";
+import Carousel from "./component/Carousel";
 import Card from "./component/Card";
 
 function App() {
   return (
     <>
       <Navbar />
-      {/* comment out because these two component have error please check that if you can and fix it */}
-      {/* <Crousel />
-      <Card /> */}
+      <Carousel />
+      <Card />
     </>
   );
 }
